ae294b0113d92401561198e1bcdf4a402fbbf51d
saugat21 <saugatbaral122@gmail.com>
2025-01-12 22:29:01 +0545

    db connected

--- backend/config/Db.js
+++ backend/config/Db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDb connected ${conn.connection.host}`);
     } catch (error) {
-        console.log("MongoDb not connected");
+        console.log("MongoDb not connecte");
         console.log(`Error:${error.message}`);
         console.error(`Stack Trace: ${error.stack}`);
         process.exit(1);
