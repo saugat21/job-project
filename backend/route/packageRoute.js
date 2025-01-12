7af337b5d87d0d43de8e3a9ef8e0dacbd4529720
saugat21 <saugatbaral122@gmail.com>
2025-01-12 17:27:25 +0545

    route done

--- backend/route/packageRoute.js
+++ backend/route/packageRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-
+import { getProducts,submitOrder } from "../controller/packageController";
 
 const router=express.Router();
 
-router.route('/').get
+router.route('/getproducts').get(getProducts);
+router.route('/submitorder').post(submitOrder)
 
 export default router;
